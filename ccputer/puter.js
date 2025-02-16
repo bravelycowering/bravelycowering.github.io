4a9273cfe72af402510b7c39a880d80dd8e7ee61
bravelycowering <bravelycowering@gmail.com>
2025-02-16 14:52:39 -0500

    a

--- ccputer/puter.js
+++ ccputer/puter.js
@@ -6,12 +6,48 @@ ctx.fillStyle = "#0000ff"
 ctx.fillRect(0, 0, 56, 48)
 ctx.fillStyle = "#ffffff"
 ctx.fillText(":( oh no", 10, 20)
-let i = 0
 input.oninput = function() {
-	ctx.fillStyle = "#0000ff"
-	ctx.fillRect(0, 0, 56, 48)
-	ctx.fillStyle = "#ffffff"
-	ctx.fillText(input.value, 10, 20)
-	ctx.fillText(i++, 10, 35)
+	key(input.value)
+	input.value = ""
 }
 input.focus()
+
+addEventListener("click", function(e) {
+	e.preventDefault()
+	e.stopPropagation()
+	e.stopImmediatePropagation()
+	input.focus()
+	if (e.clientX >= 4 && e.clientX < canvas.width + 4) {
+		if (e.clientY >= 4 && e.clientY < canvas.height + 4) {
+			click(e.clientX - 4, e.clientY - 4)
+		}
+	}
+	if (e.clientY >= 56 && e.clientY < 60 && e.clientX >= 4 && e.clientX < 8) {
+		document.getElementById("restart").remove()
+		canvas.remove()
+		setTimeout(() => {
+			location.reload()
+		}, 500)
+	}
+	if (e.clientY == 100) {
+		customEvent(e.clientX)
+	}
+})
+
+function key(k) {
+
+}
+
+function click(x, y) {
+	ctx.fillStyle = "#ff0000"
+	ctx.fillRect(x, y, 1, 1)
+}
+
+function customEvent(number) {
+	alert(number)
+	switch (number) {
+		case 0:
+
+		break;
+	}
+}
--- files/nas/os.nas
+++ files/nas/os.nas
@@ -4,12 +4,26 @@ using cef
 	if puter jump #click
 	set puter true
 	ifnot cef jump #noputer
-	msg You turn on the computer...
+	msg You turn on the computer.
+	msg Use &a/input type [text]&f to type stuff, and click on the screen to, well, click.
+	msg You can press the green pixel in the lower left to restart the computer at any time.
 	msg cef create -n s -tq bravelycowering.net/ccputer
 	msg cef resolution 64 64
 	msg cef size 4 4
-	msg cef at 69 66 59.5 270 0
+	msg cef at 68.999 66 59.5 270 0
 	tempblock 696 69 66 59
+quit
+
+#input
+	if runArg1|=|"type" jump #input-type|{runArg2}
+	else msg &cInvalid sub-command "{runArg1}"
+quit
+
+#input-type
+	ifnot puter msg &cYou need to turn the computer on first.
+	ifnot puter quit
+	msg cef type {runArg1}
+	msg cef click 0 100
 quit
 
 #noputer
