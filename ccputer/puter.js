e41be0a3abb721961aa1d371cedae8f7887f203f
bravelycowering <bravelycowering@gmail.com>
2025-02-16 16:35:41 -0500

    a

--- ccputer/puter.js
+++ ccputer/puter.js
@@ -1,11 +1,13 @@
 const canvas = document.getElementById("screen")
 const ctx = canvas.getContext("2d")
 const input = document.getElementById("input")
+const font = document.getElementById("font")
 
-ctx.fillStyle = "#0000ff"
-ctx.fillRect(0, 0, 56, 48)
-ctx.fillStyle = "#ffffff"
-ctx.fillText(":( oh no", 10, 20)
+// ctx.fillStyle = "#0000ff"
+// ctx.fillRect(0, 0, 56, 48)
+// ctx.fillStyle = "#ffffff"
+// ctx.fillText(":( oh no", 10, 20)
+
 input.oninput = function() {
 	key(input.value)
 	input.value = ""
@@ -34,20 +36,198 @@ addEventListener("click", function(e) {
 	}
 })
 
-function key(k) {
+const chars = {
+	// ROW 0
+	"A": [0., 0],
+	"B": [1., 0],
+	"C": [2., 0],
+	"D": [3., 0],
+	"E": [4., 0],
+	"F": [5., 0],
+	"G": [6., 0],
+	"H": [7., 0],
+	"I": [8., 0],
+	"J": [9., 0],
+	"K": [10, 0],
+	"L": [11, 0],
+	"M": [12, 0],
+	"N": [13, 0],
+	"O": [14, 0],
+	"P": [15, 0],
+	// ROW 1
+	"Q": [0., 1],
+	"R": [1., 1],
+	"S": [2., 1],
+	"T": [3., 1],
+	"U": [4., 1],
+	"V": [5., 1],
+	"W": [6., 1],
+	"X": [7., 1],
+	"Y": [8., 1],
+	"Z": [9., 1],
+	"<": [10, 1],
+	">": [11, 1],
+	"[": [12, 1],
+	"]": [13, 1],
+	"{": [14, 1],
+	"}": [15, 1],
+	// ROW 2
+	"a": [0., 2],
+	"b": [1., 2],
+	"c": [2., 2],
+	"d": [3., 2],
+	"e": [4., 2],
+	"f": [5., 2],
+	"g": [6., 2],
+	"h": [7., 2],
+	"i": [8., 2],
+	"j": [9., 2],
+	"k": [10, 2],
+	"l": [11, 2],
+	"m": [12, 2],
+	"n": [13, 2],
+	"o": [14, 2],
+	"p": [15, 2],
+	// ROW 3
+	"q": [0., 3],
+	"r": [1., 3],
+	"s": [2., 3],
+	"t": [3., 3],
+	"u": [4., 3],
+	"v": [5., 3],
+	"w": [6., 3],
+	"x": [7., 3],
+	"y": [8., 3],
+	"z": [9., 3],
+	"/": [10, 3],
+	"|": [11, 3],
+	"\\":[12, 3],
+	"?": [13, 3],
+	",": [14, 3],
+	".": [15, 3],
+	// ROW 4
+	"1": [0., 4],
+	"2": [1., 4],
+	"3": [2., 4],
+	"4": [3., 4],
+	"5": [4., 4],
+	"6": [5., 4],
+	"7": [6., 4],
+	"8": [7., 4],
+	"9": [8., 4],
+	"0": [9., 4],
+	":": [10, 4],
+	";": [11, 4],
+	'"': [12, 4],
+	"'": [13, 4],
+	"~": [14, 4],
+	"`": [15, 4],
+	// ROW 5
+	"!": [0., 5],
+	"@": [1., 5],
+	"#": [2., 5],
+	"$": [3., 5],
+	"%": [4., 5],
+	"^": [5., 5],
+	"&": [6., 5],
+	"*": [7., 5],
+	"(": [8., 5],
+	")": [9., 5],
+	"_": [10, 5],
+	"-": [11, 5],
+	'+': [12, 5],
+	"=": [13, 5],
+	"":  [14, 5],
+	" ": [15, 5],
+}
 
+const state = {
+	background: "#000000",
+	foreground: "#ffffff",
+	input: "",
+	sentevent: {},
+	comline: "",
+	blink: 0,
+	line: 0,
+}
+
+function drawChar(char, x, y) {
+	let c = chars[char]
+	if (c == undefined) {
+		c = chars[""]
+	}
+	ctx.fillStyle = state.background
+	ctx.fillRect(x*4, y*8, 4, 8)
+	ctx.fillStyle = state.foreground
+	ctx.drawImage(font, c[0]*4, c[1]*8, 4, 8, x*4, y*8, 4, 8)
+}
+
+function drawString(str, x, y) {
+	for (let i = 0; i < str.length; i++) {
+		drawChar(str[i], x, y)
+		x++
+		if (x >= 14) {
+			x = 0
+			y++
+		}
+	}
+	return [x, y]
+}
+
+function update() {
+	let input = state.input
+	let events = state.events
+	state.input = ""
+	state.events = {}
+	if (input.length > 0) {
+		state.comline += input
+	}
+	state.blink += 1
+	if (state.blink > 10) {
+		state.blink = 0
+	}
+}
+
+function draw() {
+	const [x, y] = drawString(state.comline, 0, state.line)
+	if (state.blink > 5) {
+		drawChar("_", x, y)
+	} else {
+		drawChar(" ", x, y)
+	}
+}
+
+function loop() {
+	update()
+	draw()
+}
+
+function key(k) {
+	state.input += k
 }
 
 function click(x, y) {
-	ctx.fillStyle = "#ff0000"
-	ctx.fillRect(x, y, 1, 1)
+	
 }
 
 function customEvent(number) {
-	alert(number)
 	switch (number) {
 		case 0:
-
+			// send
+		break;
+		case 1:
+			// up
+		break;
+		case 2:
+			// down
+		break;
+		case 3:
+			// left
+		break;
+		case 4:
+			// right
 		break;
 	}
 }
+
+setInterval(loop, 100)
