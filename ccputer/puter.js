3e20b47fb2510ddd8896ac4c71f81108ece2232b
bravelycowering <bravelycowering@gmail.com>
2025-02-16 18:25:20 -0500

    Update puter.js

--- ccputer/puter.js
+++ ccputer/puter.js
@@ -150,27 +150,63 @@ const chars = {
 	" ": [15, 5],
 }
 
+const colors = {
+	0: "#000",
+	1: "#00b",
+	2: "#0b0",
+	3: "#0bb",
+	4: "#b00",
+	5: "#b0b",
+	6: "#bb0",
+	7: "#bbb",
+	8: "#333",
+	9: "#33f",
+	10: "#3f3",
+	11: "#3ff",
+	12: "#f33",
+	13: "#f3f",
+	14: "#ff3",
+	15: "#fff",
+}
+
 const state = {
-	background: "#000000",
-	foreground: "#ffffff",
 	input: "",
 	events: {},
 	comline: "",
-	blink: 0,
+	blink: 6,
 	line: 0,
+	files: {},
+	program: [],
+	execline: 0,
+	vars: {},
 }
 
-ctx.fillStyle = state.background
-ctx.fillRect(0, 0, canvas.width, canvas.height)
+parsenum = number => {
+	let ret = parseFloat(number)
+	if (isNaN(ret)) return 0
+	return ret
+}
+
+getcolor = col => {
+	return colors[parseInt(col, 16)] || colors[parseInt(col)] || "#000"
+}
+
+state.files.autorun = `
+clear
+echo ccDOS v1.0
+set cursor = _
+set background = 0
+echo type 'help' for command info
+`.trimStart().trimEnd()
+state.program = state.files.autorun.split(/[\n\r]+/gm)
 
 function drawChar(char, x, y) {
 	let c = chars[char]
 	if (c == undefined) {
 		c = chars[""]
 	}
-	ctx.fillStyle = state.background
+	ctx.fillStyle = getcolor(state.vars["background"])
 	ctx.fillRect(x*4, y*8, 4, 8)
-	ctx.fillStyle = state.foreground
 	ctx.drawImage(font, c[0]*4, c[1]*8, 4, 8, x*4, y*8, 4, 8)
 }
 
@@ -182,49 +218,195 @@ function drawString(str, x, y) {
 	return [x, y]
 }
 
-function update() {
-	let input = state.input
-	let events = state.events
-	state.input = ""
-	state.events = {}
-	if (input.length > 0) {
-		state.comline = (state.comline + input).substring(0, 28)
+function printLine(str) {
+	drawString(str, 0, state.line)
+	state.line++
+	while (state.line > 11) {
+		state.line--
+		ctx.drawImage(canvas, 0, -8)
+		ctx.fillStyle = getcolor(state.vars["background"])
+		ctx.fillRect(0, canvas.height - 8, canvas.width, 8)
 	}
-	state.blink += 1
-	if (state.blink > 10) {
-		state.blink = 0
+}
+
+const commands = {}
+
+function doCom(com) {
+	const args = com.replace(/\%(\w+)\%/gm, function(m) {
+		return state.vars[m] || ""
+	}).split(/\s+/)
+	const cmd = args.shift()
+	const func = commands[cmd]
+	if (!func) {
+		printLine("Invalid command.")
+	} else {
+		func(...args)
 	}
-	if (events[1]) {
-		state.blink = 6
-		state.comline = state.comline.substring(0, state.comline.length - 1)
+}
+
+commands.echo = (...args) => {
+	printLine(args.join(" "))
+}
+
+commands.clear = () => {
+	ctx.fillStyle = getcolor(state.vars["background"])
+	ctx.fillRect(0, 0, canvas.width, canvas.height)
+	state.line = 0
+}
+
+commands.bg = (col) => {
+	state.vars["background"] = col
+}
+
+commands.ifset = (varname, ...com) => {
+	if (state.vars[varname] != null) {
+		doCom(com.join(" "))
 	}
-	drawString(state.comline+" ", 0, state.line)
-	if (events[0]) {
-		state.blink = 6
-		state.line++
-		state.comline = ""
-		while (state.line > 11) {
-			state.line--
-			ctx.drawImage(canvas, 0, -8)
-			ctx.fillStyle = state.background
-			ctx.fillRect(0, canvas.height - 8, canvas.width, 8)
+}
+
+commands.ifeq = (varname, value, ...com) => {
+	if (state.vars[varname] == value) {
+		doCom(com.join(" "))
+	}
+}
+
+commands.ifnset = (varname, ...com) => {
+	if (state.vars[varname] == null) {
+		doCom(com.join(" "))
+	}
+}
+
+commands.ifneq = (varname, value, ...com) => {
+	if (state.vars[varname] != value) {
+		doCom(com.join(" "))
+	}
+}
+
+commands.set = (varname, op, ...val) => {
+	if (varname == null) {
+		for (const [k, v] of Object.entries(state.vars)) {
+			printLine(k+"="+v)
+		}
+	} else {
+		if (op == null) {
+			delete state.vars[varname]
+		} else {
+			switch (op) {
+				case "=":
+					state.vars[varname] = val.join(" ")
+				break;
+				case "+=":
+					state.vars[varname] = (parsenum(state.vars[varname]) + parsenum(val.join(" "))) + ""
+				break;
+				case "-=":
+					state.vars[varname] = (parsenum(state.vars[varname]) - parsenum(val.join(" "))) + ""
+				break;
+				case "*=":
+					state.vars[varname] = (parsenum(state.vars[varname]) * parsenum(val.join(" "))) + ""
+				break;
+				case "/=":
+					state.vars[varname] = (parsenum(state.vars[varname]) / parsenum(val.join(" "))) + ""
+				break;
+				default:
+					printLine("Invalid op.")
+			}
 		}
 	}
 }
 
-function draw() {
-	const [x, y] = drawString(state.comline, 0, state.line)
-	if (state.blink > 5) {
-		drawChar("_", x, y)
-	} else {
-		drawChar(" ", x, y)
+commands.help = commands["?"] = (page) => {
+	commands.clear()
+	switch (page) {
+		default:
+			printLine("       -- help 1/3 --")
+			printLine("echo [msg...]")
+			printLine("  Prints a message")
+			printLine("clear")
+			printLine("  Clears the screen")
+			printLine("bg [color code]")
+			printLine("  Changes the background")
+		break;
+		case "2":
+		case "var":
+		case "vars":
+		case "set":
+			printLine("       -- help 2/3 --")
+			printLine("set [var] [op] [value...]")
+			printLine("  Sets a variable")
+			printLine("  Vars can be unwrapped by")
+			printLine("  surrounding them with %")
+			printLine("  op can be: = += -= *= /=")
+			printLine("set [var]")
+			printLine("  Deletes a variable")
+			printLine("set")
+			printLine("  Lists all set variables")
+		break;
+		case "3":
+		case "if":
+		case "ifeq":
+		case "ifset":
+		case "ifneq":
+		case "ifnset":
+			printLine("       -- help 3/3 --")
+			printLine("ifset [var] [com...]")
+			printLine("  Runs com if var exists")
+			printLine("ifeq [var] [val] [com...]")
+			printLine("  Runs com if var=val")
+			printLine("ifnset [var] [com...]")
+			printLine("  Wont run com if var exists")
+			printLine("ifneq [var] [val] [com...]")
+			printLine("  Wont run com if var=val")
+		break;
 	}
-	drawChar(" ", x + 1, y)
 }
 
 function loop() {
-	update()
-	draw()
+	let input = state.input
+	let events = state.events
+	state.input = ""
+	state.events = {}
+	if (state.program) {
+		const line = state.program[state.execline]
+		if (typeof(line) == "string") {
+			doCom(line)
+			state.execline++
+		} else {
+			state.program = false
+		}
+	} else {
+		if (input.length > 0) {
+			state.comline = state.comline + input
+			state.blink = 6
+		}
+		state.blink += 1
+		if (state.blink > 10) {
+			state.blink = 0
+		}
+		if (events[1]) {
+			state.blink = 6
+			state.comline = state.comline.substring(0, state.comline.length - 1)
+		}
+		if (events[0]) {
+			printLine(state.comline+" ")
+			doCom(state.comline)
+			printLine("")
+			state.blink = 6
+			state.comline = ""
+		}
+		const [x, y] = drawString(state.comline.substring(state.comline.length-27, state.comline.length), 0, state.line)
+		if (state.blink > 5) {
+			let cursor = state.vars["cursor"]
+			if (cursor == null) {
+				cursor = "_"
+			} else {
+				cursor = cursor[0] || "_"
+			}
+			drawChar(cursor, x, y)
+		} else {
+			drawChar(" ", x, y)
+		}
+		drawChar(" ", x + 1, y)
+	}
 }
 
 function key(k) {
@@ -239,4 +421,4 @@ function customEvent(number) {
 	state.events[number] = true
 }
 
-setInterval(loop, 100)
+setInterval(loop, 50)
