c5b138aa6caca514536d30d46f6d04cffcbdca27
bravelycowering <bravelycowering@gmail.com>
2026-01-11 21:34:46 -0500

    try hardcoding the loop

---
+++ nas/generateloop.lua
@@ -0,0 +1,50 @@
+local lines = {}
+
+local function li(txt)
+	lines[#lines+1] = txt
+end
+
+local lx, ly, lz = 0, 0, 0
+
+for x = -3, 3 do
+	for y = -3, 3 do
+		for z = -3, 3 do
+			if lx ~= x then
+				local dx = x - lx
+				lx = x
+li("		setadd x "..dx)
+			end
+			if ly ~= y then
+				local dy = y - ly
+				ly = y
+li("		setadd y "..dy)
+			end
+			if lz ~= z then
+				local dz = z - lz
+				lz = z
+li("		setadd z "..dz)
+			end
+			local chance = "{id}"
+			local dorandom = false
+			if math.abs(x) == 3 then
+				dorandom = true
+				chance = chance.."|7"
+			end
+			if math.abs(y) == 3 then
+				dorandom = true
+				chance = chance.."|7"
+			end
+			if math.abs(z) == 3 then
+				dorandom = true
+				chance = chance.."|7"
+			end
+			if dorandom then
+li("		setrandlist id "..chance)
+			end
+li "		if label #d[{id}] effect explosionsteamsmall {x} {y} {z} 0 0 0"
+li "		if label #d[{id}] call #setblock|0|{x}|{y}|{z}"
+		end
+	end
+end
+
+assert(io.open("loop.txt", "w+b")):write(table.concat(lines, "\n")):close()
